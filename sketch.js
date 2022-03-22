8a4ced4c2325fa7fd53e7536be165f14ed0395c2
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2022-03-22 19:50:50 +0530

    HSB color mode added

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
   angleMode(DEGREES); // canvas configs updated
+  colorMode(HSB, 360, 100, 100);
 
   // brings the point to the center
   translate(width/2, height/2);
@@ -17,11 +18,18 @@ function setup() {
   x3 = random(210,230);
   x2 = random(190,215);
   x1 = random(185,205);
-  y2 = random(5,50);
-  y3 = random(5,50);
+  let maxX2 = x2*tan(ang*0.6);
+  y2 = random(5, maxX2); // preventing overlapping
+  y3 = random(5, maxX2); // // preventing overlapping
+
+  let hue = random(256);
+  let sat = random(70, 100);
+  let brgt = random(70, 100);
 
   for (let i = 0; i < pedals; i++) {
-    noStroke();
+    
+    fill(hue, sat, brgt);
+    stroke(0,0,0);
     beginShape();
       curveVertex(x1, 0);
       curveVertex(x1, 0);
@@ -41,9 +49,9 @@ function setup() {
       curveVertex(x4, 0);
       endShape();
 
-      stroke(365);
+      stroke(hue, sat, brgt);
       strokeWeight(2);
-      line(x1,0,x4,0);
+      line(x1+3,0,x4-4,0);
 
       // only the canvas rotates
       rotate(ang);
