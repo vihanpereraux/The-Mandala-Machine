c82c3e0f2b303a8a107e01e6a64b010708e99e34
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2022-03-23 16:05:55 +0530

    Pattern completed with five iterations

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let x1, x2, x3, x4, y2, y3
-var pedals = 30;
+var pedals = 40;
 
 function setup() {
   // canvas configurations
@@ -10,51 +10,56 @@ function setup() {
 
   // brings the point to the center
   translate(width/2, height/2);
-  circle(0,0,50);
+  //circle(0,0,50);
 
   let ang = 360/pedals ;
-
-  x4 = random(230,245);
-  x3 = random(210,230);
-  x2 = random(190,215);
-  x1 = random(185,205);
-  let maxX2 = x2*tan(ang*0.6);
-  y2 = random(5, maxX2); // preventing overlapping
-  y3 = random(5, maxX2); // // preventing overlapping
 
   let hue = random(256);
   let sat = random(70, 100);
   let brgt = random(70, 100);
 
-  for (let i = 0; i < pedals; i++) {
+  let removal = 40;
+
+  for (let i = 0; i < 5; i++) {
     
-    fill(hue, sat, brgt);
-    stroke(0,0,0);
-    beginShape();
-      curveVertex(x1, 0);
-      curveVertex(x1, 0);
-      curveVertex(x2, y2);
-      curveVertex(x3, y3);
-      curveVertex(x4, 0);
-      curveVertex(x4, 0);
-      endShape();
+    x4 = random(230-i*removal,245-i*removal);
+    x3 = random(210-i*removal,230-i*removal);
+    x2 = random(190-i*removal,215-i*removal);
+    x1 = random(185-i*removal,205-i*removal);
+    let maxX2 = x2*tan(ang*0.6);
+    y2 = random(5, maxX2); // preventing overlapping
+    y3 = random(5, maxX2); // // preventing overlapping
 
-    // opposite shape
-    beginShape();
-      curveVertex(x1, 0);
-      curveVertex(x1, 0);
-      curveVertex(x2, -y2);
-      curveVertex(x3, -y3);
-      curveVertex(x4, 0);
-      curveVertex(x4, 0);
-      endShape();
+    for (let i = 0; i < pedals; i++) {
+      
+      fill(hue, sat, brgt);
+      stroke(0,0,0);
+      beginShape();
+        curveVertex(x1, 0);
+        curveVertex(x1, 0);
+        curveVertex(x2, y2);
+        curveVertex(x3, y3);
+        curveVertex(x4, 0);
+        curveVertex(x4, 0);
+        endShape();
 
-      stroke(hue, sat, brgt);
-      strokeWeight(2);
-      line(x1+3,0,x4-4,0);
+      // opposite shape
+      beginShape();
+        curveVertex(x1, 0);
+        curveVertex(x1, 0);
+        curveVertex(x2, -y2);
+        curveVertex(x3, -y3);
+        curveVertex(x4, 0);
+        curveVertex(x4, 0);
+        endShape();
 
-      // only the canvas rotates
-      rotate(ang);
+        stroke(hue, sat, brgt);
+        strokeWeight(2);
+        line(x1+3,0,x4-4,0);
+
+        // only the canvas rotates
+        rotate(ang);
+    }
   }
 }
 
